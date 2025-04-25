063632ab8be0a79bc464bab2d86774ab004ad83a
mek <mek@localhost.localdomain>
2025-04-25 19:39:42 +0300

    add css

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template_string, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return app.send_static_file('csti.html')
 
-@app.route('/ssti')
+@app.route('/ssti', methods=['GET', 'POST'])
 def ssti():
-    # Vulnerable SSTI via user-controlled template
-    template = "<h1>Hello {{ name }}</h1>"
-    return render_template_string(template, name=request.args.get('name', 'Guest'))
+    if request.method == 'POST':
+        email = request.form['email']
+        try:
+            template_value = render_template_string(email)
+            return render_template('thankyou.html', email=template_value)
+
+        except Exception as e:
+            return f"Erreur : {e}"
+
+    return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
